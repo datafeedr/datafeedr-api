1f56f0f5efb0f81fa9841b4febc7fb26281643ef
Eric <ericbusch@gmail.com>
2018-07-13 09:55:47 -0400

    Adjusted height of modal window in search form

--- datafeedr-api.php
+++ datafeedr-api.php
@@ -7,8 +7,8 @@ Author: datafeedr.com
 Author URI: https://www.datafeedr.com
 License: GPL v3
 Requires at least: 3.8
-Tested up to: 4.9.6
-Version: 1.0.85
+Tested up to: 4.9.7
+Version: 1.0.87
 
 Datafeedr API Plugin
 Copyright (C) 2018, Datafeedr - help@datafeedr.com

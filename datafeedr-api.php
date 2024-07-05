1e2d4a6a82f8fb967a7d8b2ddfdfca87615ba39e
Eric Busch <ericbusch@gmail.com>
2024-07-05 13:26:38 -0400

    Updated "tested up to" value

--- datafeedr-api.php
+++ datafeedr-api.php
@@ -9,8 +9,8 @@ Text Domain: datafeedr-api
 License: GPL v3
 Requires PHP: 7.4
 Requires at least: 3.8
-Tested up to: 6.4.4-alpha
-Version: 1.3.17
+Tested up to: 6.7-alpha-58674
+Version: 1.3.18
 
 Datafeedr API Plugin
 Copyright (C) 2024, Datafeedr - help@datafeedr.com
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Define constants.
  */
-define( 'DFRAPI_VERSION', '1.3.17' );
+define( 'DFRAPI_VERSION', '1.3.18' );
 define( 'DFRAPI_URL', plugin_dir_url( __FILE__ ) ); // https://example.com/wp-content/plugins/datafeedr-api/
 define( 'DFRAPI_PATH', plugin_dir_path( __FILE__ ) ); // /absolute/path/to/wp-content/plugins/datafeedr-api/
 define( 'DFRAPI_BASENAME', plugin_basename( __FILE__ ) ); // datafeedr-api/datafeedr-api.php
